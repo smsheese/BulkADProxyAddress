803f7fe048d33294f099f3495f465c55661bb368
Sheese Sheikh <58876239+smsheese@users.noreply.github.com>
2023-12-20 12:55:53 +0530

    commits scripts 1.1

---
+++ fetch_AD_proxyAddresses.ps1
@@ -0,0 +1,7 @@
+﻿Set-ExecutionPolicy RemoteSigned
+# Import Active Directory module
+Import-Module ActiveDirectory
+
+# Fetch users from AD and export to CSV
+$users = Get-ADUser -Filter * -Properties ProxyAddresses
+$users | Select-Object Name, SamAccountName, @{Name='ProxyAddresses';Expression={($_.ProxyAddresses -join ";")}} | Export-Csv -Path "C:\Temp\AD_users_proxyAddresses.csv" -NoTypeInformation
---
+++ update_AD_proxyAddresses.ps1
@@ -0,0 +1,25 @@
+﻿Set-ExecutionPolicy RemoteSigned
+Import-Module ActiveDirectory
+
+# Read the updated CSV and update users in AD
+$updatedUsers = Import-Csv -Path "C:\Temp\AD_users_proxyAddresses.csv"
+foreach ($user in $updatedUsers) {
+    # Check if the ProxyAddresses column is not empty
+    if (![string]::IsNullOrWhiteSpace($user.ProxyAddresses)) {
+        try {
+            # Split the ProxyAddresses by ";" and filter out any empty entries
+            $proxyAddresses = $user.ProxyAddresses -split ";" | Where-Object { $_ -ne "" }
+            # Ensure that each proxy address has the correct format
+            $proxyAddresses = $proxyAddresses | ForEach-Object { if ($_ -notmatch '^smtp:') { "smtp:$_" } else { $_ } }
+            
+            # Convert $proxyAddresses to a string before updating
+            $proxyAddressesAsString = $proxyAddresses -join ";"
+
+            Set-ADUser -Identity $user.SamAccountName -Replace @{ProxyAddresses=$proxyAddressesAsString}
+        } catch {
+            Write-Error "Failed to update user $($user.SamAccountName): $_"
+        }
+    } else {
+        Write-Host "No ProxyAddresses to update for $($user.SamAccountName)"
+    }
+}
